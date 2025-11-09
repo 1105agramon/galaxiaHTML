4a0558f7008eb972814daa9e72303f4deecc329f
Juan Granados <111211763+1105agramon@users.noreply.github.com>
2025-11-09 16:51:12 -0600

    actualizacion

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ for (let i = 0; i < phraseCount; i++) {
     
     // Ajuste de Escala Dinámico
     if (resource.length > 4 && resource.includes('/')) {
-        sprite.scale.set(5, 5, 1); // Escala para imágenes
+        sprite.scale.set(20, 20, 1); // Escala para imágenes
     } else if (resource.length <= 2) {
         sprite.scale.set(3, 3, 1); // Escala para emojis
     } else {
