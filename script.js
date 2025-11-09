69bb8c684a527433fdd1dc45c3a020405cea53e4
Juan Granados <111211763+1105agramon@users.noreply.github.com>
2025-11-09 00:25:16 -0600

    color

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function createTextTexture(text) {
     canvas.width = 1024; // Resolución de la textura
     canvas.height = 128;
     context.font = `bold ${fontSize}px Arial`;
-    context.fillStyle = 'rgba(255, 255, 255, 0.67)';
+    context.fillStyle = 'rgba(148, 0, 0, 1)';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
     context.fillText(text, canvas.width / 2, canvas.height / 2);
